dcdce57dac2d43dc245544cd179f1561e24c2420
astra-the-boop <astra.the.boop@icloud.com>
2026-02-03 18:11:14 +0700

    the thingyamajig

---
+++ thingy.py
@@ -0,0 +1,14 @@
+import mss
+import time
+import mss.tools
+
+t = 30
+
+print("recording")
+with mss.mss() as sct:
+    monitor = sct.monitors[1]
+    for i in range(2*t):
+        screenshot = sct.grab(monitor)
+        mss.tools.to_png(screenshot.rgb, screenshot.size, output=f"screens/{i}.png")
+        time.sleep(0.5)
+
