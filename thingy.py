29386b1a5e9e91fe95061cfc464c3ceede0ba848
astra-the-boop <astra.the.boop@icloud.com>
2026-02-04 15:03:20 +0700

    i'll figure out the remove dir shit later

--- thingy.py
+++ thingy.py
@@ -2,16 +2,19 @@ import mss
 import time
 import mss.tools
 import main
+import os
 
 t = 30
 
+os.remove("screens/0.png")
+
 print("recording")
-# with mss.mss() as sct:
-#     monitor = sct.monitors[1]
-#     for i in range(2*t):
-#         screenshot = sct.grab(monitor)
-#         mss.tools.to_png(screenshot.rgb, screenshot.size, output=f"screens/{i}.png")
-#         time.sleep(0.5)
+with mss.mss() as sct:
+    monitor = sct.monitors[1]
+    for i in range(2*t):
+        screenshot = sct.grab(monitor)
+        mss.tools.to_png(screenshot.rgb, screenshot.size, output=f"screens/{i}.png")
+        time.sleep(0.5)
 
 for i in range(int(main.getVidLength("bad-apple.mp4")*10)):
     main.render("bad-apple.mp4", f"{i}", t=i/100)
