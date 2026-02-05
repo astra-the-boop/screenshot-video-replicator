df7421db48645576bf330b75d2d501f707cbd676
astra-the-boop <astra.the.boop@icloud.com>
2026-02-05 17:43:07 +0700

    apple bad

--- thingy.py
+++ thingy.py
@@ -4,6 +4,7 @@ import mss.tools
 import main
 import os
 import shutil
+import moviepy.video.io.ImageSequenceClip
 
 def record(t, dir="screens"):
     try:
@@ -18,6 +19,15 @@ def record(t, dir="screens"):
             mss.tools.to_png(screenshot.rgb, screenshot.size, output=f"{dir}/{i}.png")
             time.sleep(0.5)
 
-def render(inVid="bad-apple.mp4"):
-    for i in range(int(main.getVidLength(input)*10)):
-        main.render(input, f"{i}", t=i/10)
+def renderFrames(inVid="bad-apple.mp4", dir="", fps=10):
+    for i in range(int(main.getVidLength(inVid)*fps)):
+        main.render(inVid, f"{dir}{"/" if dir else ""}{i}", t=i/fps)
+
+def render(dir="", fps=10):
+    frames = [f for f in os.listdir(".") if os.path.isfile(f) and f.endswith(".png")]
+    frames.sort()
+    # print(frames)
+    clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(frames, fps)
+    clip.write_videofile("output.mp4")
+
+render()
