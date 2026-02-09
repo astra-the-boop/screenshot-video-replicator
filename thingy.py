fe18a3a0aa5c5c4753f89a42d3780af97625a5d9
astra-the-boop <astra.the.boop@icloud.com>
2026-02-09 09:25:55 +0700

    pbar for record

--- thingy.py
+++ thingy.py
@@ -5,6 +5,7 @@ import main
 import os
 import shutil
 import moviepy.video.io.ImageSequenceClip
+from tqdm import tqdm
 
 def record(t, dir="screens", remove=True):
     if remove:
@@ -19,10 +20,10 @@ def record(t, dir="screens", remove=True):
         shutil.rmtree("screens")
     except OSError as e:
         print(f"{e.strerror}")
-    print("recording")
+
     with mss.mss() as sct:
         monitor = sct.monitors[1]
-        for i in range(2*t):
+        for i in tqdm(range(2*t), desc="Recording screen...", unit=" screenshots"):
             screenshot = sct.grab(monitor)
             try:
                 mss.tools.to_png(screenshot.rgb, screenshot.size, output=f"{dir}/{i}.png")
@@ -47,7 +48,5 @@ def render(fps=10, output="output.mp4"):
     frames = [int(f[:-4]) for f in os.listdir(".") if os.path.isfile(f) and f.endswith(".png")]
     frames.sort()
     frameName = [str(i)+".png" for i in frames]
-    # print(frameName)
-    # print(frames)
     clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(frameName, fps)
     clip.write_videofile(output)
